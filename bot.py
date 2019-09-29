e8b37fb45912ff737d4659eb9a4bf41b4dd521b1
Ojas Sethi <sethi.ojas@gmail.com>
2019-09-29 21:07:41 +0530

    Major Changes
    1)Added option to choose backgroud colour via inline keyboard.
    2)Callback function Squares the images as per the choice of user(bg colour).
    3)Square_It Function saves the picture locally, provides choice to users and
    calls the 'callback' function.
    4)File path is stored in a file instead of varibale.
    5)Locally stored image and file containing the associated local path is
    automatically deleted at the end.

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler
 from telegram.ext import MessageHandler, Filters
 import os
 import square
+import telegram
 
 updater = Updater(token='TOKEN', use_context=True)
 dispatcher = updater.dispatcher
+
 
 def start(update, context):
 	''' Replies with a Generic mesage to /start and /help commands'''
@@ -13,29 +15,60 @@ def start(update, context):
 		"square it for you!")
 
 def Square_It(update, context):
-	''' Replies with a Squared Image of the photo which is sent by the user '''
+	''' Saves picture locally and asks the user for the colour of background '''
 	
 	#Download photo
 	image = context.bot.getFile(update.message.photo[-1].file_id)
-	file_name = os.path.join(os.getcwd(), f"{image.file_id}.jpg")
-	image.download(custom_path = file_name)
-
-	#Square the picture
-	square.square_image(file_name)
-
-	#Open the picture and send it
-	file = open(file_name, 'rb')
-	update.message.reply_photo(file)
-	file.close()
-	os.remove(file_name)
+	FILE_NAME = os.path.join(os.getcwd(), f"{image.file_id}.jpg")
+	image.download(custom_path = FILE_NAME)
 	
+	#save path in file
+	with open("name.txt", 'w') as f:
+		f.write(FILE_NAME)
+
+	custom_keyboard = [[telegram.InlineKeyboardButton('White', callback_data = 'White')],
+ 					[telegram.InlineKeyboardButton('Black', callback_data = 'Black')]]
+	
+	reply_markup = telegram.InlineKeyboardMarkup(custom_keyboard)
+	
+	context.bot.send_message(chat_id=update.message.chat_id,
+	                 text="Please choose the background colour",
+	                 reply_markup=reply_markup)
+
+def callback(update, context):
+	'''
+	Sends the square image according to the choice.
+	Deletes the local copy of the picture
+	'''
+
+	query = update.callback_query
+	colour = query.data
+
+	query.edit_message_text(text=f"Selected option: {colour}")
+    
+    #get File path
+	with open("name.txt", 'r') as f:
+		FILE_NAME = f.read()
+	FILE_NAME = FILE_NAME.strip()
+   
+	square.square_image(FILE_NAME, colour)
+    
+	file = open(FILE_NAME, 'rb')
+	context.bot.send_photo(caption = "Here you go!", chat_id = query.message.chat_id, photo = file)
+   
+	file.close()
+	os.remove(FILE_NAME)
+	os.remove('name.txt')
+
 #Create Handlers
 start_handler = CommandHandler(['start', 'help'], start)
 photo_handler = MessageHandler(Filters.photo, Square_It)
+callback_handler = CallbackQueryHandler(callback)
 
 #Deploy Handlers
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(photo_handler)
+dispatcher.add_handler(callback_handler)
 
 #Check For updates
 updater.start_polling()
