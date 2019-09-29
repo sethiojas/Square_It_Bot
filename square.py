b01a4aaae8cd86bd24ada45ba54bc4175d4879b7
Ojas Sethi <sethi.ojas@gmail.com>
2019-09-29 21:06:18 +0530

    Only black and white option available for colour
    "Auto" option removed

--- square.py
+++ square.py
@@ -4,31 +4,15 @@ import os
 def square_image(filename, colour):
 	''' 
 	Squares the image whose path is received.
-	The padding is according to the user choice.
+	The padding is according to the user choice (Black/White).
 	'''
 	im = Image.open(filename)
 	size = max(im.size)
 	x, y= im.size
 	if colour.lower() == "white":
 		RGB = (255, 255, 255)
-	else if colour.lower() == "black":
+	elif colour.lower() == "black":
 		RGB = (0, 0, 0)
-	else:
-		red = 0
-		green = 0
-		blue = 0
-		count = 0
-
-		for r,g,b in im.getdata():
-			red = red + r
-			blue = blue + b
-			green = green + g
-
-		red = int(red/count)
-		blue = int(blue/count)
-		green = int(green/count)
-
-		RGB = (red, green, blue)
 
 	new_im = Image.new('RGB', (size, size), RGB)
 	new_im.paste(im, (int((size - x) / 2), int((size - y) / 2)))
